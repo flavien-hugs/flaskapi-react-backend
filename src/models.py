4aa98f2d26cd149b227a2929d7d860c50072c955
flavien-hugs <geekactux@gmail.com>
2023-01-13 16:48:35 +0000

    Create and edit resource for API models

---
+++ src/models.py
@@ -0,0 +1,42 @@
+from . import db
+
+from datetime import datetime
+
+
+"""
+class Recipe:
+    id: int primary key
+    rcp_title: str
+    rcp_desc: str (text)
+    rcp_created: date
+    rcp_updated: date
+"""
+
+
+class Updateable:
+    def update(self, data):
+        for attr, value in data.items():
+            setattr(self, attr, value)
+
+
+class Recipe(Updateable, db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    rcp_title = db.Column(db.String(80), nullable=False)
+    rcp_desc = db.Column(db.Text(), nullable=False)
+    rcp_created = db.Column(db.DateTime, default=datetime.utcnow())
+    rcp_updated = db.Column(db.DateTime, onupdate=datetime.utcnow())
+
+    def __repr__(self):
+        return f"<Recipe self.rcp_title>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, rcp_title, rcp_desc):
+        self.rcp_title = rcp_title
+        self.rcp_desc = rcp_desc
---
+++ src/resources.py
@@ -0,0 +1,10 @@
+from flask_restx import fields
+
+from . import api
+
+
+recipe_resource_model = api.model('Recipe', {
+    'id': fields.Integer(readonly=True, description='The recipe unique identifier'),
+    'rcp_title': fields.String(required=True, description='The recipe title'),
+    'rcp_desc': fields.String(required=True, description='The recipe description'),
+})
